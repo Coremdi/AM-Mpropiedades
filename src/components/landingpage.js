10a4a252672b35729520756ddc992f9bb43dbce7
Pablocort94 <pablocort94@hotmail.com>
2025-05-22 19:50:17 -0300

    Added styling modifications

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import './landingpage.css';
-import { Link } from 'react-router-dom'; // Assuming you're using react-router
+import { Link } from 'react-router-dom';
 
 const LandingPage = () => {
   return (
     <div className="landing-container">
-      <h2 className="buynrent-title">
-        Bienvenido a AM&M Propiedades, donde cada propiedad encuentra su propósito.
-      </h2>
+      <h1 className="landing-title">Bienvenido a AM&M Propiedades</h1>
+      <h2 className="landing-subtitle">Donde cada propiedad encuentra su propósito</h2>
       <Link to="/house-screener" className="buynrent-button">
         Encontrá la tuya
       </Link>
     </div>
   );
 };
- 
+
 export default LandingPage;
